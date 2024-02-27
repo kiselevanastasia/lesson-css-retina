0c41be7803ccca67537922b9dc452746a9b501b4
kiselevanastasia <sisnastasia@mail.ru>
2024-02-27 19:02:27 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
 import React from 'react';
 import cat from '../src/assets/images/cat-1.jpg'
-import cat2 from '../src/assets/images/cat-2.jpg'
+import cat2 from '../src/assets/images/cat@2x.png'
+// import cat3 from '../src/assets/images/cat@3x.png'
 
 
 function App() {
@@ -9,8 +10,14 @@ function App() {
         <div className="App">
             <p>Hello, Retina &#128075;</p>
 
-            {/*<div className={'bg'}></div>*/}
-            {/*<img src={cat2} alt="cat"/>*/}
+            {/* <div className={'bg'}></div> */}
+            <img src={cat} srcSet={`${cat2} 2x`} alt="cat"/>
+            {/* <img src={cat} srcSet={`${cat2} 2x, ${cat3} 3x`} alt="cat"/> */}
+
+            <picture>
+                <source srcSet={`${cat2} 2x`} />
+                <img src={cat2} alt="" />
+            </picture>
 
         </div>
     );
